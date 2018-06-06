9e77e401dd0a81c3d3f143ebda5b630212389da2
gabriel <00068810@uca.edu.sv>
2018-06-06 16:36:47 -0600

    Yet another test.

--- DICSASACRM/Form1.vb
+++ DICSASACRM/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
     Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
         MessageBox.Show("Hola mundo!")
+
         Me.Close()
     End Sub
 End Class
